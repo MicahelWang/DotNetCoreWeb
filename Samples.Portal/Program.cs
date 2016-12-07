8dc7b504af4c6932a0f9dcbeaed2f88b0e3a6467
Michael Wang <490811331@qq.com>
2016-12-07 11:04:39 +0800

    update url

--- Samples.Portal/Program.cs
+++ Samples.Portal/Program.cs
@@ -14,6 +14,7 @@ namespace WebApplication
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls("http://*:80")
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
